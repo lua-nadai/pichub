708e725c3e39f289b1d12f4122e38fdb33b27d37
Luana Nadai <lnadai@leroymerlin.com.br>
2023-12-07 16:43:52 -0300

    feat/v1: Navbar and Footer components created and imported into routes. Added default styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Home from './pages/Home';
 import About from './pages/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import './App.css';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Navbar />
       <div className='container'>
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/about' element={<About />}/>
         </Routes>
       </div>
+      <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,26 @@
+import { NavLink } from 'react-router-dom'
+import './index.css'
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+      <NavLink to='/' className='brand'>
+        Pic <span>Hub</span>
+      </NavLink>
+      <ul className='links_list'>
+        <li>
+          <NavLink to='/'>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'>
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
