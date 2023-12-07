d74f6ba71b1b23ebd952a1a42d0cc3bd06fa9364
Luana Nadai <lnadai@leroymerlin.com.br>
2023-12-07 14:31:45 -0300

    feat/v1: Project initialization and cleanup. Firebase installation

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeiC-o1gHYppRs3_AoRNSRyK0ubPWUYaU",
+  authDomain: "pichub-aff31.firebaseapp.com",
+  projectId: "pichub-aff31",
+  storageBucket: "pichub-aff31.appspot.com",
+  messagingSenderId: "358082640893",
+  appId: "1:358082640893:web:83d7aa3dd9ac3f40ef7794"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db }
